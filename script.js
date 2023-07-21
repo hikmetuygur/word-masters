c05d27c570172c4fae5de644ca06a929609cc5b8
Hikmet Uygur <uygurhikmet@gmail.com>
2023-07-21 18:46:22 +0300

    Add commit function
    commit() -> correct, close and wrong parts done.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const letters = document.querySelectorAll('.letter');
-const loadingDiv = document.querySelector('.info-bar');
+const letters = document.querySelectorAll(".letter");
+const loadingDiv = document.querySelector(".info-bar");
 const ANSWER_LENGTH = 5;
 
 async function init() {
-  let currentGuess = '';
+  let currentGuess = "";
   let currentRow = 0;
 
-  const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day?");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
   setLoading(false);
 
   console.log(word);
@@ -19,12 +20,9 @@ async function init() {
       currentGuess += letter;
     } else {
       //replace the last letter
-      currentGuess =
-        currentGuess.substring(0, currentGuess.length - 1) + letter;
+      currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
-
-    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText =
-      letter;
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
   }
 
   async function commit() {
@@ -36,24 +34,44 @@ async function init() {
     // TODO validate the word
 
     // TODO do all marking and correct, close, wrong
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        //do nothing we already did it
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
 
     // TODO did they win or lose
 
     currentRow++;
-    currentGuess = '';
+    currentGuess = "";
   }
 
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
-    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = '';
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
   }
 
-  document.addEventListener('keydown', function handleKeyPress(e) {
+  document.addEventListener("keydown", function handleKeyPress(e) {
     const action = e.key;
 
-    if (action === 'Enter') {
+    if (action === "Enter") {
       commit();
-    } else if (action === 'Backspace') {
+    } else if (action === "Backspace") {
       backspace();
     } else if (isLetter) {
       addLetter(action.toUpperCase());
@@ -68,7 +86,20 @@ function isLetter(letter) {
 }
 
 function setLoading(isLoading) {
-  loadingDiv.classList.toggle('show', isLoading);
+  loadingDiv.classList.toggle("show", isLoading);
+}
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
 
 init();
